eb3fe3b94809883922fbc3478e1294262f9f1bba
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-29 18:25:03 +0530

    Add WeatherDisplay component with conditional styling

--- src/components/WeatherDisplay.jsx
+++ src/components/WeatherDisplay.jsx
@@ -1,1 +1,21 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  // Step 2: Conditional inline styling
+  const tempColor = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      {/* p tag for temperature */}
+      <p>
+        Temperature: <span style={{ color: tempColor }}>{weather.temperature}</span>
+      </p>
+
+      {/* p tag for conditions */}
+      <p>Conditions: {weather.conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
 
